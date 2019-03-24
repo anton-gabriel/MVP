70b0aee8c0a203abd798424af99206ac2fd04d4e
Gabriel <gabi1998anton1998@gmail.com>
2019-03-24 12:17:54 +0200

    Modified vertical arrangement.

--- TotalCommander/TotalCommander/MainWindow.xaml.cs
+++ TotalCommander/TotalCommander/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace TotalCommander
@@ -8,9 +9,32 @@ namespace TotalCommander
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<ColumnDefinition> columnDefinitions;
+        List<RowDefinition> rowDefinitions;
+        bool verticalArrangement;
+
+        private void InitializeGridDefinitions()
+        {
+            this.columnDefinitions = new List<ColumnDefinition>();
+            this.rowDefinitions = new List<RowDefinition>();
+
+            this.columnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+            this.columnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(5, GridUnitType.Pixel) });
+            this.columnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+            this.rowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+            this.rowDefinitions.Add(new RowDefinition() { Height = new GridLength(5, GridUnitType.Pixel) });
+            this.rowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeGridDefinitions();
+            foreach (ColumnDefinition columnDefinition in this.columnDefinitions)
+            {
+                this.CentralGrid.ColumnDefinitions.Add(columnDefinition);
+            }
+            this.verticalArrangement = false;
         }
 
         private void LeftComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -20,19 +44,33 @@ namespace TotalCommander
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //Model.ListViewManager.Instance.Back();
-           
-                if (contentControl1.Template == Resources["gridVertical"])
-                    contentControl1.Template = (ControlTemplate)Resources["gridHorizontal"];
-                else
-                    contentControl1.Template = (ControlTemplate)Resources["gridVertical"];
-            
+            if (this.verticalArrangement)
+            {
+                this.CentralGrid.RowDefinitions.Clear();
+                foreach (ColumnDefinition columnDefinition in this.columnDefinitions)
+                {
+                    this.CentralGrid.ColumnDefinitions.Add(columnDefinition);
+                }
+                this.verticalArrangement = false;
+
+            }
+            else
+            {
+                this.CentralGrid.ColumnDefinitions.Clear();
+                foreach (RowDefinition rowDefinition in this.rowDefinitions)
+                {
+                    this.CentralGrid.RowDefinitions.Add(rowDefinition);
+                }
+                this.verticalArrangement = true;
+            }
+
+
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-           // Model.ListViewManager.Instance.Next();
+            // Model.ListViewManager.Instance.Next();
         }
-     
+
     }
 }
